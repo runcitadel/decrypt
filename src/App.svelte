366846e4c3fd9333b2d531f26cd3895fba797ef7
Aaron Dewes <aaron.dewes@web.de>
2021-11-04 15:31:12 +0000

    Add Ciadel/Umbrel switch

--- src/App.svelte
+++ src/App.svelte
@@ -33,10 +33,19 @@
 			newUserJSON = "Invalid data!";
 		}
 	}
+
+	function switchNode() {
+		let codeBlocks = document.querySelectorAll("code");
+		codeBlocks.forEach((codeblock) => {
+			codeblock.innerText = codeblock.innerText
+					.replace("umbrel", document.querySelector('input[name="node"]:checked').value)
+					.replace("citadel", document.querySelector('input[name="node"]:checked').value);
+		})
+	}
 </script>
 
 <main>
-	<h1>Umbrel & Citadel seed decryptor and encryptor</h1>
+	<h1>user.json utils</h1>
 	<p>This app decrypts the seed in our browser. No data is send over to our servers.</p>
 	<input placeholder='Encrypted seed without ""' bind:value={seed}>
 	<input placeholder="password" bind:value={password} type="password">
@@ -46,9 +55,13 @@
 	<br>
 	<h1>Reset your password</h1>
 	<p>Forgot your password, but still have your 24 words? Reset your password here!</p>
+	<input type="radio" name="node" on:click={switchNode} value="umbrel" id="umbrel" class="inline">
+	<label for="umbrel" class="inline">Umbrel</label>
+	<input type="radio" name="node" on:click={switchNode} value="citadel" id="citadel" class="inline" checked="true">
+	<label for="umbrel" class="inline">Citadel</label>
 	<p>First, turn off your node (unplug it) and reflash the SD card. This will reset the SSH password to "moneyprintergobrrr".</p>
 	<p>Then, SSH into your node and run this command:</p>
-	<p><code>cat ~/umbrel/db/user.json</code></p>
+	<p><code>cat ~/citadel/db/user.json</code></p>
 	<p>Then copy the output and paste it here:</p>
 	<textarea placeholder='user.json' bind:value={userJSON}></textarea>
 	<p>Now, enter your 24 words here (separated by spaces, all lowercase). If you don't have these anymore, no worries, but the display 24 words option on the dashboard won't work after this change again and you won't be able to get them anymore.</p>
@@ -56,7 +69,7 @@
 	<p>Finally, choose a new password:</p>
 	<input placeholder="New password" bind:value={newPassword} type="password">
 	<p><button on:click={updateUserJson}>Update user.json</button></p>
-	<p>To apply these changes, run <code>nano ~/umbrel/db/user.json</code>, delete everything in the file, and put this into it:</p>
+	<p>To apply these changes, run <code>nano ~/citadel/db/user.json</code>, delete everything in the file, and put this into it:</p>
 	<textarea bind:value={newUserJSON} readonly></textarea>
 	<p>Then save with CTRL+X and try to log in again on the dashboard.</p>
 
@@ -100,5 +113,9 @@
 		width: 100%;
 		margin: 0;
 		box-sizing: border-box;
+	}
+
+	.inline {
+		display: inline;
 	}
 </style>
